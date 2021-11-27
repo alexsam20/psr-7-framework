83a9cc72de6079ba81b300b28b72555b39e9cfe1
Alexandr <alsam3637@gmail.com>
2021-11-27 18:14:26 -0500

    Method render()

--- src/App/Http/Action/HelloAction.php
+++ src/App/Http/Action/HelloAction.php
@@ -10,10 +10,16 @@ class HelloAction
     public function __invoke(ServerRequestInterface $request)
     {
         $name = $request->getQueryParams()['name'] ?? 'Guest';
+        return new HtmlResponse($this->render($name));
+    }
 
+    /**
+     * @return string
+     */
+    private function render($name): string
+    {
         ob_start();
         require 'templates/hello.php';
-        $html = ob_get_clean();
-        return new HtmlResponse($html);
+        return ob_get_clean();
     }
 }

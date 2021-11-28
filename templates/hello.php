a8ffada7c05899cbb01ea03217561ebf6e5d3a93
Alexandr <alsam3637@gmail.com>
2021-11-28 17:23:30 -0500

    HelloActionTest

--- templates/hello.php
+++ templates/hello.php
@@ -40,7 +40,7 @@
     <main class="container">
 
         <div class="jumbotron bg-light">
-            <h1>Hello, <?php echo htmlspecialchars($name) ?>!</h1>
+            <h1>Hello!</h1>
             <p>
                 Congratulations! You have successfully created you application.
             </p>
--- tests/App/Http/Action/HelloActionTest.php
+++ tests/App/Http/Action/HelloActionTest.php
@@ -4,9 +4,9 @@ namespace Tests\App\Http\Action;
 
 use App\Http\Action\HelloAction;
 use Framework\Template\TemplateRenderer;
-use Zend\Diactoros\ServerRequest;
+use PHPUnit\Framework\TestCase;
 
-class HelloActionTest
+class HelloActionTest extends TestCase
 {
     private $renderer;
 
@@ -16,26 +16,12 @@ class HelloActionTest
         $this->renderer = new TemplateRenderer('templates');
     }
 
-    public function testGuest()
+    public function test()
     {
         $action = new HelloAction($this->renderer);
-
-        $request = new ServerRequest();
-        $response = $action($request);
+        $response = $action();
 
         self::assertEquals(200, $response->getStstusCode());
-        self::assertContains('Hello, Guest!', $response->getBody()->getContents());
-    }
-
-    public function testJson()
-    {
-        $action = new HelloAction($this->renderer);
-
-        $request = (new ServerRequest())
-            ->withQueryParams(['name' => 'John']);
-
-        $response = $action($request);
-
-        self::assertContains('Hello, John!', $response->getBody()->getContents());
+        self::assertContains('Hello!', $response->getBody()->getContents());
     }
 }

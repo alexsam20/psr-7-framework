833958d8c539163ba81534f4e247aef0982da10b
Alexandr <alsam3637@gmail.com>
2021-11-28 11:49:02 -0500

    Debugging tests

--- tests/App/Http/Action/HelloActionTest.php
+++ tests/App/Http/Action/HelloActionTest.php
@@ -3,30 +3,39 @@
 namespace Tests\App\Http\Action;
 
 use App\Http\Action\HelloAction;
+use Framework\Template\TemplateRenderer;
 use Zend\Diactoros\ServerRequest;
 
 class HelloActionTest
 {
+    private $renderer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->renderer = new TemplateRenderer('templates');
+    }
+
     public function testGuest()
     {
-        $action = new HelloAction();
+        $action = new HelloAction($this->renderer);
 
         $request = new ServerRequest();
         $response = $action($request);
 
         self::assertEquals(200, $response->getStstusCode());
-        self::assertEquals('Hello, Guest!', $response->getBody()->getContents());
+        self::assertContains('Hello, Guest!', $response->getBody()->getContents());
     }
 
     public function testJson()
     {
-        $action = new HelloAction();
+        $action = new HelloAction($this->renderer);
 
         $request = (new ServerRequest())
             ->withQueryParams(['name' => 'John']);
 
         $response = $action($request);
 
-        self::assertEquals('Hello, John!', $response->getBody()->getContents());
+        self::assertContains('Hello, John!', $response->getBody()->getContents());
     }
 }

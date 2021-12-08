8a0c6c6558686c95f8ccc26b6dd98091330d0ad3
Alexandr <alsam3637@gmail.com>
2021-12-08 11:21:35 -0500

    Submission to templates.global.php

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -6,8 +6,6 @@ use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
 use Framework\Template\TemplateRenderer;
-use Framework\Template\Twig\Extension\RouteExtension;
-use Framework\Template\Twig\TwigRenderer;
 use Psr\Container\ContainerInterface;
 
 return [
@@ -36,47 +34,8 @@ return [
                     $container->get(TemplateRenderer::class)
                 );
             },
-            TemplateRenderer::class => function (ContainerInterface $container) {
-                return new TwigRenderer(
-                    $container->get(Twig\Environment::class),
-                    $container->get('config')['templates']['extension']
-                );
-            },
-            Twig\Environment::class => function(ContainerInterface $container)
-            {
-                $debug = $container->get('config')['debug'];
-                $templateDir = $container->get('config')['twig']['template_dir'];
-                $config = $container->get('config')['twig'];
-
-                $loader = new Twig\Loader\FilesystemLoader();
-                $loader->addPath($templateDir);
-
-                $environment = new Twig\Environment($loader, [
-                    'cache' => $debug ? false : $config['cache_dir'],
-                    'debug' => $debug,
-                    'strict_variables' => $debug,
-                    'auto_reload' => $debug,
-                ]);
-
-                if ($debug) {
-                    $environment->addExtension(new Twig\Extension\DebugExtension());
-                }
-
-                $environment->addExtension($container->get(RouteExtension::class));
-
-                return $environment;
-            },
         ],
     ],
 
     'debug' => false,
-
-    'templates' => [
-        'extension' => '.html.twig',
-    ],
-    
-    'twig' => [
-        'template_dir' => 'templates',
-        'cache_dir' => 'var/cache/twig'
-    ],
 ];
